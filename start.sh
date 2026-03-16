4be73a46a6c44903675ed6ba19438cd7e071d139
ke <dtkcble795@hotmail.com>
2026-03-16 09:06:18 +0000

    i

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/keindex1/Kronos.git
 cd ./Kronos
 pip install -r requirements.txt
+modelscope download --model AI-ModelScope/Kronos-base --local_dir ./pretrained/Kronos-base
+modelscope download --model AI-ModelScope/Kronos-Tokenizer-base --local_dir ./pretrained/Kronos-Tokenizer-base
 python train_sequential.py --config ./configs/hmd_002947_kline_5min.yaml
-cp -r ./finetuned /mnt/data/
